f983090844c299b5c41a8f9a8b5ee8835508c4b0
harshanooka <harshavardhannooka@gmail.com>
2016-06-20 11:10:06 -0700

    fix(module-api): fix IIFE and button to close movieinfo screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,37 @@
 /**
  * Created by harsh on 6/12/2016.
  */
+(function () {
+    var app = angular.module('movieflix',['ngRoute','ui.bootstrap','angular-input-stars']).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+        when('/',{
+            templateUrl:'public/html/fullContent.html',
+            controller: 'browseController'
+        }).
+        when('/movies', {
+            templateUrl:'public/html/movies.html',
+            controller: 'browseController'
+        }).
+        when('/tv-shows',{
+            templateUrl:'public/html/shows.html',
+            controller: 'browseController'
+        }).
+        when('/admin',{
+            templateUrl:'public/html/editTitles.html',
+            controller: 'browseController'
+        })
 
-var app = angular.module('movieflix',['ngRoute','ui.bootstrap','angular-input-stars']).
-config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-   when('/',{
-        templateUrl:'public/html/fullContent.html',
-        controller: 'browseController'
-    }).
-    when('/movies', {
-        templateUrl:'public/html/movies.html',
-        controller: 'browseController'
-    }).
-    when('/tv-shows',{
-        templateUrl:'public/html/shows.html',
-        controller: 'browseController'
-    }).
-    when('/admin',{
-        templateUrl:'public/html/editTitles.html',
-        controllr: 'browseController'
-    })
-
-       .otherwise({redirectTo: '/'});
+            .otherwise({redirectTo: '/'});
 
 
-}]);
+    }]);
 
-app.filter('startFrom', function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
-    }
-});
+    app.filter('startFrom', function() {
+        return function(input, start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+    });
+
+})();
--- public/js/controllers/browseController.js
+++ public/js/controllers/browseController.js
@@ -1,112 +1,119 @@
-app.controller('browseController', function($rootScope, $scope, $http, $uibModal) {
+(function () {
+    angular.module('movieflix').controller('browseController', function($rootScope, $scope, $http, $uibModal) {
 
-  $rootScope.movielist;
-  $scope.newTitle = {};
-  $http.get('data/movielist.json').success(function(data) {
-    $rootScope.movielist = data;
+        $rootScope.movielist;
+        $scope.newTitle = {};
+        $http.get('data/movielist.json').success(function(data) {
+            $rootScope.movielist = data;
 
-  });
-
-
-  //Pagination
-  $scope.maxSize = 12;
-  $scope.currentPage = 1;
-
-  $scope.animationsEnabled = true;
-
-  $scope.login = function() {
-    var loginModalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'public/html/signin.html',
-      controller: loginModalInstanceController
-    })
-
-  };
-
-  $scope.add = function () {
-      $scope.newTitle = {};
-      var addTitleModalInstance = $uibModal.open({
-          animation: $scope.animationsEnabled,
-          templateUrl: 'public/html/addTitle.html',
-          controller: addTitleModalInstanceController,
-          resolve: {
-              movielist: function () {
-                  return $rootScope.movielist;
-              }
-          }
-      })
-  };
+        });
 
 
+        //Pagination
+        $scope.maxSize = 12;
+        $scope.currentPage = 1;
 
-  $scope.movieInfo = function (item) {
-      $scope.item = item;
-      var movieModalInstance = $uibModal.open({
-          animation: $scope.animationsEnabled,
-          templateUrl:'public/html/movieInfo.html',
-          controller: modalInstanceController,
-          resolve: {
-              item: function () {
-                  return $scope.item;
-              }
-          }
-      });
-  };
+        $scope.animationsEnabled = true;
 
-  $scope.editTitle = function (item) {
-      $scope.item = item;
-      var movieModalInstance = $uibModal.open({
-          animation: $scope.animationsEnabled,
-          templateUrl:'public/html/adminMovieInfo.html',
-          controller: modalInstanceController,
-          resolve: {
-              item: function () {
-                  return $scope.item;
-              }
-          }
-      });
-  }
-    
+        $scope.login = function() {
+            var loginModalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'public/html/signin.html',
+                controller: loginModalInstanceController
+            })
 
-  
-});
+        };
+
+        $scope.add = function () {
+            $scope.newTitle = {};
+            var addTitleModalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'public/html/addTitle.html',
+                controller: addTitleModalInstanceController,
+                resolve: {
+                    movielist: function () {
+                        return $rootScope.movielist;
+                    }
+                }
+            })
+        };
 
 
 
-var modalInstanceController = function ($scope, $rootScope, $uibModalInstance, item) {
-    $scope.item = item;
+        $scope.movieInfo = function (item) {
+            $scope.item = item;
+            var movieModalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl:'public/html/movieInfo.html',
+                controller: modalInstanceController,
+                resolve: {
+                    item: function () {
+                        return $scope.item;
+                    }
+                }
+            });
+        };
 
-    $scope.saveEdit = function () {
-        $uibModalInstance.dismiss('cancel');
-    }
+        $scope.editTitle = function (item) {
+            $scope.item = item;
+            var movieModalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl:'public/html/adminMovieInfo.html',
+                controller: modalInstanceController,
+                resolve: {
+                    item: function () {
+                        return $scope.item;
+                    }
+                }
+            });
+        }
 
-    $scope.delete = function (item) {
-        var index = $rootScope.movielist.indexOf(item);
-        $rootScope.movielist.splice(index,1);
-        $uibModalInstance.dismiss('cancel');
 
-    }
 
-}
+    });
 
-var loginModalInstanceController = function ($scope, $uibModalInstance, $location) {
-    
-    $scope.cancel = function() {
-        $uibModalInstance.dismiss('cancel');
-    };
-}
 
-var addTitleModalInstanceController = function ($scope, $uibModalInstance, $rootScope) {
 
-    $scope.save = function (newListing) {
-        $rootScope.movielist.push(newListing);
-         $uibModalInstance.dismiss();
+    var modalInstanceController = function ($scope, $rootScope, $uibModalInstance, item) {
+        $scope.item = item;
+
+        $scope.saveEdit = function () {
+            $uibModalInstance.dismiss('cancel');
+        }
+
+        $scope.delete = function (item) {
+            var index = $rootScope.movielist.indexOf(item);
+            $rootScope.movielist.splice(index,1);
+            $uibModalInstance.dismiss('cancel');
+
+        }
+
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
 
     };
 
-    $scope.cancel = function() {
-        $uibModalInstance.dismiss('cancel');
+    var loginModalInstanceController = function ($scope, $uibModalInstance, $location) {
+
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
     };
-}
+
+    var addTitleModalInstanceController = function ($scope, $uibModalInstance, $rootScope) {
+
+        $scope.save = function (newListing) {
+            $rootScope.movielist.push(newListing);
+            $uibModalInstance.dismiss();
+
+        };
+
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+    };
 
 
+
+})();
